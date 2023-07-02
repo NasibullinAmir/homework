6a01b2e6bf99800272f9280aa6ab18963de8c34f
NasibullinAmir <savageamir16@mail.ru>
2023-07-02 23:59:17 +0500

    test

---
+++ hw2/hw2/hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  hw2App.swift
+//  hw2
+//
+//  Created by karim on 02.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
